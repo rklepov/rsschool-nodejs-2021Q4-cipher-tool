6960f4a385fd081a3868c4574f6fc2841dfbc201
Roman Klepov <rklepov@gmail.com>
2021-11-21 21:01:44 +0300

    fix: disable strings decoding for writable stream

--- src/streams.js
+++ src/streams.js
@@ -58,7 +58,7 @@ class Input extends Readable {
  */
 class Output extends Writable {
     constructor(filename) {
-        super({ defaultEncoding: "utf8" }); // TODO: writes text in fixed encoding only
+        super({ decodeStrings: false, defaultEncoding: "utf8" }); // TODO: writes text in fixed encoding only
         this.filename = filename;
     }
 
