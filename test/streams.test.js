21b9748a4a0fd349541d4e9920ff5a18be7a9fed
Roman Klepov <rklepov@gmail.com>
2021-11-21 21:07:27 +0300

    test: add tests for transform stream correct usage scenarios

--- test/streams.test.js
+++ test/streams.test.js
@@ -1,9 +1,55 @@
 // streams.test.js
 
-// import { jest } from "@jest/globals";
+import { jest } from "@jest/globals";
+
+import { Readable, Writable } from "stream";
+import { pipeline } from "stream/promises";
 
 import Stream from "../src/streams.js";
 import Except from "../src/except.js";
+
+/**
+ * Fake implementation of a writable stream which collects the data in the
+ * internal buffer.
+ */
+class MockWriteable extends Writable {
+    constructor() {
+        super({ decodeStrings: false, defaultEncoding: "utf8" });
+        this.buffer = "";
+    }
+
+    _construct(callback) {
+        callback();
+    }
+
+    _write(chunk, _, callback) {
+        this.buffer += chunk;
+        callback();
+    }
+}
+
+/**
+ * Fake implementation of a readable stream which publishes the fixed text.
+ */
+class MockReadable extends Readable {
+    constructor(text) {
+        super({ encoding: "utf8" });
+        this.text = text;
+    }
+
+    _construct(callback) {
+        callback();
+    }
+
+    _read(n) {
+        this.push(this.text);
+        this.push(null);
+    }
+
+    _destroy(err, callback) {
+        callback(err);
+    }
+}
 
 describe("Transform stream", () => {
     describe("Incorrect config", () => {
@@ -21,6 +67,25 @@ describe("Transform stream", () => {
             expect(f).toThrow(msg);
         });
     }); // Incorrect config
+
+    describe("Usage examples", () => {
+        const configOutput = [
+            ["C1-C1-R0-A", 'Myxn xn nbdobm. Tbnnfzb ferlm "_" nhteru!'],
+            ["C1-C0-A-R1-R0-A-R0-R0-C1-A", 'Vhgw gw wkmxkv. Ckwwoik onauv "_" wqcnad!'],
+            ["A-A-A-R1-R0-R0-R0-C1-C1-A", 'Hvwg wg gsqfsh. Asggous opcih "_" gmapcz!'],
+            ["C1-R1-C0-C0-A-R0-R1-R1-A-C1", 'This is secret. Message about "_" symbol!'],
+        ];
+
+        test.each(configOutput)("Correct config passed: %j", async (spec, output) => {
+            const mockReadStream = new MockReadable('This is secret. Message about "_" symbol!');
+            const mockWriteStream = new MockWriteable();
+            let encdec = Stream.EncodeDecode.createFromConfig(spec);
+
+            await pipeline(mockReadStream, ...encdec, mockWriteStream);
+
+            expect(mockWriteStream.buffer).toBe(output);
+        });
+    });
 });
 
 //__EOF__
